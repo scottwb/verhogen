942bd21d268b9221713ef64e1c87d6c24ed768a1
Scott W. Bradley <scottwb@scottwb.net>
2012-03-02 18:11:03 -0800

    Turn on Express logging.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
